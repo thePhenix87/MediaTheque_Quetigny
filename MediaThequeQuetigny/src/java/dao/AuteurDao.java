fb7e6682cf804640317df1e41a2138211e735bb7
thePhenix87 <forex.ksiba@gmail.com>
2016-11-23 13:25:57 +0100

    Creation DAO

---
+++ MediaThequeQuetigny/src/java/dao/AuteurDao.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import model.Auteur;
+
+/**
+ *
+ * @author Tarik
+ */
+public class AuteurDao extends DAO_IMPL<Auteur>{
+
+    public AuteurDao() {
+       super(Auteur.class);
+    }
+    
+    
+}
