2c37a3c1be9f552f8e84ad9e8e9033a606f1e8f6
alex-dev <alex-dev@GS30-2M-Shadow>
2016-11-23 14:31:05 +0100

    test index

---
+++ MediaThequeQuetigny/src/java/dao/EmpruntDao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import model.Emprunt;
+
+/**
+ *
+ * @author alex-dev
+ */
+public class EmpruntDao extends DAO_IMPL<Emprunt>{
+    
+    
+    public EmpruntDao()
+    {
+        super(Emprunt.class);
+    }
+    
+}
