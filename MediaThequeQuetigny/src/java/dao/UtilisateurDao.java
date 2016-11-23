81970e0b7baa18bde0e6acffde667a1d19b24957
samb51 <samuel.bilcocq@gmail.com>
2016-11-23 13:48:55 +0100

    test

---
+++ MediaThequeQuetigny/src/java/dao/UtilisateurDao.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import model.Utilisateur;
+
+/**
+ *
+ * @author Samuel
+ */
+public class UtilisateurDao extends DAO_IMPL<Utilisateur>{
+    
+    public UtilisateurDao()
+    {
+        super(Utilisateur.class);
+    }
+}
