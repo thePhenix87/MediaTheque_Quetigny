c5e4eec4705d8b32e529d0cc0586b2590fe11796
Baptou-Solide <sp.rouillon@gmail.com>
2016-11-29 15:13:04 +0100

    Ajout de la gestion utilisateur

--- MediaThequeQuetigny/src/java/controller/controllerAdministrateur.java
+++ MediaThequeQuetigny/src/java/controller/controllerAdministrateur.java
@@ -5,21 +5,113 @@
  */
 package controller;
 
+import dao.UtilisateurDao;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
 import model.Utilisateur;
 
 /**
  *
  * @author onesh
  */
-public class controllerAdministrateur {
+@Named
+@SessionScoped
+public class controllerAdministrateur implements Serializable {
     
     
-    private Utilisateur util;
-    
+    private Utilisateur util,utilselected;
+    @Inject
+    private UtilisateurDao utildao;
+    private List<Utilisateur> listUtil;
+    private HashMap param;
     
     public controllerAdministrateur(){
+       util = new Utilisateur();
         
     }
+
+     public void rechercherUtil(){
+        utildao.find(Integer.SIZE);
+    }
+    
+     public void supprimerUtil(Utilisateur u){
+         System.out.println("rentre dans supprimer util");
+         System.err.println(u);
+         utildao.delete(u);
+     }
+    
+     public void ajouterUtil(Utilisateur utilselected){
+         System.out.println("rentre dans ajouter util");
+         utildao.create(utilselected);
+     }
+     
+     public void modifierUtil(Utilisateur utilselected){
+         System.out.println("rentre dans modifier util");
+         utildao.update(utilselected);
+     }
+     
+    /**
+     * @return the util
+     */
+    public Utilisateur getUtil() {
+        return util;
+    }
+
+    /**
+     * @param util the util to set
+     */
+    public void setUtil(Utilisateur util) {
+        this.util = util;
+    }
+
+    /**
+     * @return the utildao
+     */
+    public UtilisateurDao getUtildao() {
+        return utildao;
+    }
+
+    /**
+     * @param utildao the utildao to set
+     */
+    public void setUtildao(UtilisateurDao utildao) {
+        this.utildao = utildao;
+    }
+
+    /**
+     * @return the listUtil
+     */
+    public List<Utilisateur> getListUtil() {
+        return utildao.getAll();
+    }
+
+    /**
+     * @param listUtil the listUtil to set
+     */
+    public void setListUtil(List<Utilisateur> listUtil) {
+        this.listUtil = listUtil;
+    }
+
+    /**
+     * @return the utilselected
+     */
+    public Utilisateur getUtilselected() {
+        return utilselected;
+    }
+
+    /**
+     * @param utilselected the utilselected to set
+     */
+    public void setUtilselected(Utilisateur utilselected) {
+        this.utilselected = utilselected;
+    }
+    
+    
     
     
     
